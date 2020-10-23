c510cf0545abf87d72607d11d34be6f32809d034
yanjingyun <326015540@qq.com>
2020-10-23 17:41:34 +0800

    mybatis动态sql参数重新配置

--- springboot-test25/src/main/java/com/yjy/user/mapper/UserMapperProvider.java
+++ springboot-test25/src/main/java/com/yjy/user/mapper/UserMapperProvider.java
@@ -7,18 +7,19 @@ import com.yjy.user.entity.User;
 public class UserMapperProvider {
 
 	public String findByUser(User user) {
-		StringBuilder sb = new StringBuilder("select * from tb_user t where 1 = 1");
+		String sql = "select * from tb_user t";
 		
+		StringBuilder sb = new StringBuilder("");
 		// username 模糊查询
 		if (!StringUtils.isEmpty(user.getUsername())) {
-			sb.append(" and t.username like ").append("'%").append(user.getUsername()).append("%'");
+			sb.append(" and t.username like CONCAT('%',#{username},'%')");
 		}
 		
 		// age 大于
 		if (user.getAge() != null) {
-			sb.append(" and t.age >= ").append(user.getAge());
+			sb.append(" and t.age >= #{age}");
 		}
 		
-		return sb.toString();
+		return (sb.length() > 0) ? (sql + " where 1 = 1" + sb.toString()) : sql;
 	}
 }
