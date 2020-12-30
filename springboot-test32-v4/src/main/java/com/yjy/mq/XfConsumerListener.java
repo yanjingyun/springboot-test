948306d1a7b5ee2b6154abedc3f180fc276b7b76
yanjingyun <326015540@qq.com>
2020-12-30 18:50:47 +0800

    feat(整合rocketmq): +1

--- springboot-test32-v4/src/main/java/com/yjy/mq/XfConsumerListener.java
+++ springboot-test32-v4/src/main/java/com/yjy/mq/XfConsumerListener.java
@@ -21,6 +21,8 @@ public class XfConsumerListener implements RocketMQListener<String>, RocketMQPus
         // 每次拉取的间隔，单位为毫秒
         consumer.setPullInterval(2000);
         // 设置每次从队列中拉取的消息数为2
+        // 每次从每个队列的拉取数，Consumer每次拉取消息总数=所有broker上队列数 * pullBatchSize
+        // 例如，2个broker，每个broker的队列数为4，每次从队列拉取数为3，则consumer每次拉取消息总数=2*4*3=24条
         consumer.setPullBatchSize(2);
     }
 }
