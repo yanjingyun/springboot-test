905f50c2031943c6b4aa2aa90fa7f4ff7b8fec58
yanjingyun <326015540@qq.com>
2021-02-21 20:43:48 +0800

    fix(validator): 修复手动校验全部正确时，StringBuilder去掉最后一个字符报错问题

--- springboot-test28/src/main/java/com/yjy/util/ValidatorUtil.java
+++ springboot-test28/src/main/java/com/yjy/util/ValidatorUtil.java
@@ -17,30 +17,40 @@ import java.util.Set;
  */
 public class ValidatorUtil {
 
-    private static Validator validator =  Validation.buildDefaultValidatorFactory().getValidator();
+    private static Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
-    public static <T> String validateEntity(T obj){
+    public static <T> String validateEntity(T obj) {
         StringBuilder stringBuilder = new StringBuilder();
         Set<ConstraintViolation<T>> set = validator.validate(obj);
-        if(!CollectionUtils.isEmpty(set) ){
-            for(ConstraintViolation<T> cv : set){
+        if (!CollectionUtils.isEmpty(set)) {
+            for (ConstraintViolation<T> cv : set) {
                 stringBuilder.append(cv.getPropertyPath()).append(cv.getMessage()).append("，");
             }
         }
-        return stringBuilder.deleteCharAt(stringBuilder.length() - 1).toString();
+
+        return stringBuilder.length() == 0 ? "" : stringBuilder.deleteCharAt(stringBuilder.length() - 1).toString();
     }
 
     // 测试
     public static void main(String[] args) {
         // 测试1
         UserVo userVo1 = new UserVo();
-        System.out.println(ValidatorUtil.validateEntity(userVo1));
+        System.out.println("userVo1校验：" + ValidatorUtil.validateEntity(userVo1));
 
         // 测试2
         UserVo userVo2 = new UserVo();
         userVo2.setAdress(new UserVo.Adress() {{
             setName("11");
         }});
-        System.out.println(ValidatorUtil.validateEntity(userVo2));
+        System.out.println("userVo2校验：" + ValidatorUtil.validateEntity(userVo2));
+
+        // 测试3
+        UserVo userVo3 = new UserVo();
+        userVo3.setName("axxs");
+        userVo3.setMobilePhone("1234566");
+        userVo3.setAdress(new UserVo.Adress() {{
+            setName("123");
+        }});
+        System.out.println("userVo3校验：" + ValidatorUtil.validateEntity(userVo3));
     }
 }
